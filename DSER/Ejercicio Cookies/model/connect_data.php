06c7048667c039d9e722650c1120528d7fb5402d
Mikel Granero <mikelgmh5b09@gmail.com>
2018-11-12 09:52:19 +0100

    Ejercicio Cookies

---
+++ DSER/Ejercicio Cookies/model/connect_data.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class connect_data
+{
+    public $host='raspberrymikel.ddns.net';
+    public $userbbdd='root';
+    public $passbbdd='95761438';
+    public $ddbbname='2aw3_users_cookies';
+}
+
