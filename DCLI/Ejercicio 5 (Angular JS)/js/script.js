2d95c575ed4db6f39303332f0b6957e86749ce6d
Mikel Granero <mikelgmh5b09@gmail.com>
2018-11-27 10:09:25 +0100

    Ejemplo Angular JS (DCLI Ej 5)

---
+++ DCLI/Ejercicio 5 (Angular JS)/js/script.js
@@ -0,0 +1,10 @@
+var miApp = angular.module("miApp", []);
+miApp.controller("miControlador",function($scope) {
+    $scope.nombre = "";
+    $scope.apellido = "";
+    $scope.mensaje = "";
+
+    $scope.SacarMensaje = function () {
+        $scope.mensaje = "Hola " + $scope.nombre + "" + $scope.apellido;
+    }
+});
