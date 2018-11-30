1694d06cfa68e945fe04edc918d7df4a5e7a8f23
Mikel Granero <mikelgmh5b09@gmail.com>
2018-11-30 10:06:27 +0100

    Dos combobox funcionando pero con ng-options (Es mejor con el repeat)

--- DCLI/Ejercicio 7 (Angular, 2 combos)/js/script.js
+++ DCLI/Ejercicio 7 (Angular, 2 combos)/js/script.js
@@ -5,11 +5,18 @@ miApp.controller('miControlador', function ($scope, $http) {
     $scope.listaMainCombobox = [];
     $scope.listaSecondCombobox = [];
     $scope.mostrarSegundoSelect = 'false';
+    $scope.autor = '';
+    $scope.titulo = '';
 
     $scope.resultPrimerSelect = {
         id: "",
         tipo: "",
         formato: ""
+    }
+    $scope.resultSegundoSelect = {
+        titulo: "",
+        autor: "",
+        tipo: ""
     }
 
     $http.get('JSON/datoscombo1.json').then(function (data) {
@@ -17,11 +24,16 @@ miApp.controller('miControlador', function ($scope, $http) {
     });
 
     $scope.mostrarSelect = function () {
-            $http.get('JSON/datoscombo1.json').then(function (data) {
-        $scope.listaMainCombobox = data.data;
-    });
-        $scope.mostrarSegundoSelect = 'true';
+    var idJSON = $scope.resultPrimerSelect.id;
 
+    // Pide el JSON
+        $http.get('JSON/datos_' + idJSON + '.json').then(function (data) {
+        $scope.listaSecondCombobox = data.data;
+    });
+
+    $scope.mostrarSegundoSelect = 'true';
+       $scope.titulo = $scope.resultSegundoSelect.titulo;
+       $scope.autor = $scope.resultSegundoSelect.autor;
     };
 
 
