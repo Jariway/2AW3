49b2232671a65d936fb3b88444c4c797d86dcf98
Mikel Granero <mikelgmh5b09@gmail.com>
2019-02-06 10:53:48 +0100

    haciendo CSS angular

--- DCLI/Ejercicio 8 (Angular, Tabla BD)/js/Cargardatos.js
+++ DCLI/Ejercicio 8 (Angular, Tabla BD)/js/Cargardatos.js
@@ -1,1 +1,10 @@
 
+var miAplicacion = angular.module('miAplicacion', []);
+
+miAplicacion.controller('mainController', ["$scope", "$http", function ($scope, $http) {
+
+    $scope.showAdd = 1;
+    $scope.showEdit = 1;
+    $scope.showDelete = 1;
+    $scope.showConsultas = 1;
+}]);
