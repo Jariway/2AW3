04ff12abc431b2d013da398ffb50680f357bbe7a
BuildTools <unconfigured@null.spigotmc.org>
2019-02-08 15:12:46 +0100

    Haciendo el ejercicio de jQuery desde 0, estilos del login listos

--- DIW/Examen 29-01-2019/Froga_ikasle IWEB RETO 3/js/script_optimizado.js
+++ DIW/Examen 29-01-2019/Froga_ikasle IWEB RETO 3/js/script_optimizado.js
@@ -6,40 +6,6 @@ $(document).ready(function () {
     var height = $(window).height();
     event.preventDefault();
 
-    // Lista de todos los libros (Sólo se carga una vez)
-    var todosLibros;
-
-    // Botón login muestra el login
-    $(".loginbutton").click(function () {
-        $("#idDivLogin").css("display", "grid");
-    });
-
-    // Botón cancelar oculta el login
-    $("#cancelar").click(function () {
-        $("#idDivLogin").css("display", "none");
-    });
-
-    // Al hacer click en biblioteca muestra los libros
-    $("#bibliotecaNav").click(function () {
-        $(".container").css("display", "flex");
-        //$(".container").css("display", "flex");
-        obtenerLibros();
-    });
-
-    // Botón de enviar
-    $("#submitUser").click(function () {
-        var erabiltzailea = $("#userLogin").val();
-        var pasahitza = $("#passLogin").val();
-        if (erabiltzailea == "Ane" && pasahitza == "Usuario12") {
-            $("#idDivLogin").css("display", "none");
-            $("#bibliotecaNav").css("display", "flex");
-        } else {
-            alert("Contraseña incorrecta")
-        }
-    });
-
-
-
 
 
 });
