180d0cd9bff677dcb9eb4dda876a1660fbe7dceb
BuildTools <unconfigured@null.spigotmc.org>
2019-02-08 16:55:00 +0100

    Cuando haces bien el login te muestra arriba el menú biblioteca

--- DIW/Examen 29-01-2019/Froga_ikasle IWEB RETO 3/js/script_optimizado.js
+++ DIW/Examen 29-01-2019/Froga_ikasle IWEB RETO 3/js/script_optimizado.js
@@ -4,8 +4,28 @@ $(document).ready(function () {
     $('body').height(height);
     $('body').css("background-color", "grey");
     var height = $(window).height();
-    event.preventDefault();
+    //event.preventDefault();
 
+    // ACCIONES
 
+    // Botón login muestra el login
+    $(".loginbutton").click(function () {
+        $("#idDivLogin").css("display", "grid");
+    });
+
+    $(".cancelar").click(function () {
+        $("#idDivLogin").css("display", "none");
+    });
+
+    $(".enviar").click(function () {
+        var erabiltzailea = $(".usuario").val();
+        var pasahitza = $(".pass").val();
+        if (erabiltzailea == "Ane" && pasahitza == "Usuario12") {
+            $("#idDivLogin").css("display", "none");
+            $("#bibliotecaNav").css("display", "grid");
+        } else {
+            alert("Contraseña incorrecta")
+        }
+    });
 
 });
