6e0bb3889da69ca8c18850545706e126423134d6
BuildTools <unconfigured@null.spigotmc.org>
2019-02-16 11:09:10 +0100

    No me gusta hecho, cambiar entre pelicula honena y pelikula zerrenda

--- DIW/Ejercicio Películas/script.js
+++ DIW/Ejercicio Películas/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
 
+    // Eventos
     $(".mg").click(function () {
         var previousNumber = parseInt($(this).text());
-        $(this).text(previousNumber+1);
+        $(this).text(previousNumber + 1);
+    });
 
+    $(".nmg").click(function () {
+        var previousNumber = parseInt($(this).text());
+        $(this).text(previousNumber + 1);
+    });
+
+    $("#honena").click(function () {
+        $("table").css("display", "none");
+        $(".pelikulaonena").css("display", "block");
+    });
+    
+    $("#zerrenda").click(function () {
+        $("table").css("display", "table");
+        $(".pelikulaonena").css("display", "none");
     });
 });
