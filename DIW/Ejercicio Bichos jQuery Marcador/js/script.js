699915aa289c9969f72f8e705b20bf03767e7437
BuildTools <unconfigured@null.spigotmc.org>
2019-02-16 09:36:55 +0100

    Ejercicio bichos 2 terminado

--- DIW/Ejercicio Bichos jQuery Marcador/js/script.js
+++ DIW/Ejercicio Bichos jQuery Marcador/js/script.js
@@ -8,6 +8,12 @@ $(document).ready(function () {
     $("#puntuar").click(function () {
         $(".bichos").css("display", "grid");
     });
+    $("#cambiarEstilos").click(function () {
+        var valorSelect = getValorSelect();
+        var varlorTamanio = parseInt( $("#tamanio").val());
+        var varlorColor = $("#color").val();
+        changeStyles(valorSelect, varlorTamanio, varlorColor);
+    });
     $("#mejor").click(function () {
         var highestImage = getHighest();
         showHighestImage(highestImage);
@@ -17,10 +23,21 @@ $(document).ready(function () {
     });
 
     // Funciones
+    function changeStyles(valorSelect, varlorTamanio, varlorColor) {
+        if (valorSelect == "TITULO") {
+            $(".titulo").css("color",varlorColor);
+            $(".titulo").css("font-size", varlorTamanio);
+        }else{
+            $("img").css("border", "2px solid" + varlorColor);
+            $("img").css("padding", varlorTamanio);
+        }
+    }
+
     function addPoint(pid) {
         $(".p" + pid).text(parseInt($(".p" + pid).text()) + 1);
     }
 
+    // Obtiene la imagen con el número más alto
     function getHighest() {
         var highestNumber = 0;
         var highest = 1;
@@ -35,9 +52,15 @@ $(document).ready(function () {
         return highest;
     }
 
-    function showHighestImage(pnumber){
+    // Muestra la imagen con la puntuación más alta
+    function showHighestImage(pnumber) {
         var srcImagen = $("#i" + pnumber).attr('src');
         $("#mejorImagen").css("display", "block");
         $("#mejorImagen").attr("src", srcImagen);
+    }
+
+    // Obtiene el valor del select de los estilos
+    function getValorSelect() {
+        return $("#aplicara option:selected").text();
     }
 });
